299f5e013d33b5358e8a968b015f15ec724b129a
janusbirgisson <janusbirgisson@gmail.com>
2023-03-03 09:13:17 +0000

    byrjun

---
+++ StonePaperScissors.java
@@ -0,0 +1,7 @@
+public class StonePaperScissors {
+    public static void main(String[] args) {
+
+        int player1 = (int) (Math.random() * 3);
+        int player2 = (int) (Math.random() * 3);
+
+    }}
